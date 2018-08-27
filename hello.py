4e3662c3275671d84723e1e11a7e7d306aa9f2db
Sidolux <midas78@gmail.com>
2018-08-27 18:06:57 +0200

    v3

--- hello.py
+++ hello.py
@@ -1,8 +1,3 @@
 print("Hello World")
-test = "test1"
-
+test: str = "test1"
 print(test)
-while ans:
-    question = raw_input("Ask the magic 8 ball a question: (press enter to quit) ")
-answers = random.randint(1, 8)
-if question == "":
